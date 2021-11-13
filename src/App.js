143ca2c203fdb000a6739476ff6ae7bdaa682878
TIMPICKLE <NIHAODONG1621@gmail.com>
2021-11-13 20:45:00 +0800

    在package.json文件中增加一个homepage字段：
    "homepage": ".",

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
-// import './App.css';
-// import '../src/pic_src/s2.jpg'
-// import React, {useEffect, useState} from "react";
-//
-//
-// function App() {
-//     const [n,setN] = useState("click")
-//
-//     const show=()=>{
-//        console.log("fuck")
-//        let c = document.getElementById('canvas');
-//        let ctx=c.getContext("2d");
-//        let img = new Image();
-//        img.src = "../src/pic_src/test.png";
-//        ctx.drawImage(img,360,640)
-//        setN("has been clicked");
-//
-//
-//     }
-//     useEffect(()=>{console.log("get")},[n]);
-//
-//   return (
-//     <div className="App">
-//         <div className="cover">
-//             <canvas id="canvas"  >
-//
-//             </canvas>
-//             <br></br>
-//         <button onClick={show} type="button" value="">
-//
-//             click
-//         </button>
-//         </div>
-//     </div>
-//   );
-// }
-//
-//
-//
-// export default App;
+import React from 'react';
+
+import './index.css';
+import './App.css';
+
+import imgUrl from '../src/bc.png'
+
+
+function App() {
+    const show=()=>{
+
+        let img1 = new Image(); // Image 构造器
+        img1.src = imgUrl;
+        img1.alt = 'alt';
+        //document.body.appendChild(img1);
+        let ctx = document.getElementById('canvas').getContext('2d');
+        // let div = document.getElementsByClassName('cover')
+        ctx.drawImage(img1,0,0,img1.width,img1.height);
+    }
+
+
+    return (
+        <div className="App">
+            <div className="cover">
+                <canvas id="canvas" width='360' height='640'   >
+
+                </canvas>
+                <br></br>
+                <button onClick={show} type="button" value="">
+
+                    click
+                </button>
+            </div>
+        </div>
+    );
+}
+ export default App;
--- src/index.js
+++ src/index.js
@@ -3,37 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
  import './App.css';
 import reportWebVitals from './reportWebVitals';
-import imgUrl from '../src/bc.png'
+
+import App from "./App";
 
 
-function App() {
-    const show=()=>{
-
-        let img1 = new Image(); // Image 构造器
-        img1.src = imgUrl;
-        img1.alt = 'alt';
-        //document.body.appendChild(img1);
-        let ctx = document.getElementById('canvas').getContext('2d');
-        // let div = document.getElementsByClassName('cover')
-        ctx.drawImage(img1,0,0,img1.width,img1.height);
-    }
-
-
-    return (
-        <div className="App">
-            <div className="cover">
-                <canvas id="canvas" width='360' height='640'   >
-
-                </canvas>
-                <br></br>
-                <button onClick={show} type="button" value="">
-
-                    click
-                </button>
-            </div>
-        </div>
-    );
-}
 
 
 ReactDOM.render(
