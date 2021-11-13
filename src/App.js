f4efe2a43bd1b0660f7e7420ad249ee51cf4149a
TIMPICKLE <NIHAODONG1621@gmail.com>
2021-11-13 19:34:04 +0800

    原来不能用css来设置canvas的宽高  会被拉伸

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+// import './App.css';
+// import '../src/pic_src/s2.jpg'
+// import React, {useEffect, useState} from "react";
+//
+//
+// function App() {
+//     const [n,setN] = useState("click")
+//
+//     const show=()=>{
+//        console.log("fuck")
+//        let c = document.getElementById('canvas');
+//        let ctx=c.getContext("2d");
+//        let img = new Image();
+//        img.src = "../src/pic_src/test.png";
+//        ctx.drawImage(img,360,640)
+//        setN("has been clicked");
+//
+//
+//     }
+//     useEffect(()=>{console.log("get")},[n]);
+//
+//   return (
+//     <div className="App">
+//         <div className="cover">
+//             <canvas id="canvas"  >
+//
+//             </canvas>
+//             <br></br>
+//         <button onClick={show} type="button" value="">
+//
+//             click
+//         </button>
+//         </div>
+//     </div>
+//   );
+// }
+//
+//
+//
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+ import './App.css';
 import reportWebVitals from './reportWebVitals';
+import imgUrl from '../src/bc.png'
+
+
+function App() {
+
+
+    const show=()=>{
+
+        let img1 = new Image(); // Image 构造器
+        img1.src = imgUrl;
+        img1.alt = 'alt';
+        //document.body.appendChild(img1);
+        let ctx = document.getElementById('canvas').getContext('2d');
+        // let div = document.getElementsByClassName('cover')
+        ctx.drawImage(img1,0,0,img1.width,img1.height);
+    }
+
+
+    return (
+        <div className="App">
+            <div className="cover">
+                <canvas id="canvas" width='360' height='640'   >
+
+                </canvas>
+                <br></br>
+                <button onClick={show} type="button" value="">
+
+                    click
+                </button>
+            </div>
+        </div>
+    );
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +45,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
